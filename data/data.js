1340bc43420117388f540f6d43a8a5d415d5ef06
M.C. van den Boogaart <scola@kvdb.name>
2016-07-01 15:01:48 +0200

    Add data

--- data/data.js
+++ data/data.js
@@ -1,7 +1,17 @@
+import de_DE from './de_DE';
 import en_GB from './en_GB';
+import en_US from './en_US';
+import es_ES from './es_ES';
+import fr_FR from './fr_FR';
+import hi_IN from './hi_IN';
 import nl_NL from './nl_NL';
 
 export default {
+  de_DE,
   en_GB,
+  en_US,
+  es_ES,
+  fr_FR,
+  hi_IN,
   nl_NL
 };
