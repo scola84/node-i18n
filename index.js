e5522da9eec5ea2d4f051aa6dcd13f8262db92f1
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:30:59 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-export { default as CurrencyFormat } from './src/currency';
-export { default as DateFormat } from './src/date';
-export { default as I18n } from './src/i18n';
-export { default as NumberFormat } from './src/number';
-export { default as StringFormat } from './src/string';
+import CurrencyFormat from './src/currency';
+import DateFormat from './src/date';
+import I18n from './src/i18n';
+import NumberFormat from './src/number';
+import StringFormat from './src/string';
+
+export {
+  CurrencyFormat,
+  DateFormat,
+  I18n,
+  NumberFormat,
+  StringFormat
+};
